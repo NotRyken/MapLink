4880a4402b52bf0b67c11cdfb8ab2bcd20e5a96d
NotRyken <127091011+NotRyken@users.noreply.github.com>
2026-07-10 19:06:38 +0800

    update to latest xaero's mods

--- common/src/main/java/de/the_build_craft/maplink/common/AbstractModInitializer.java
+++ common/src/main/java/de/the_build_craft/maplink/common/AbstractModInitializer.java
@@ -66,7 +66,7 @@ public abstract class AbstractModInitializer
 {
 	public static final String MOD_ID = "maplink";
 	public static final String MOD_NAME = "Map Link";
-	public static final String VERSION = "4.4.0";
+	public static final String VERSION = "4.4.1";
 	public static final Logger LOGGER = LogManager.getLogger("MapLink");
 	public static AbstractModInitializer INSTANCE;
 	public LoaderType loaderType;
